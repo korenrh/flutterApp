7f04dedc825dd44c047b24871e31c90eca50201f
Reilly Koren <reillykoren@Reillys-MBP.attlocal.net>
2020-10-13 17:24:40 -0500

    Firebase

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+export var firebaseConfig = {
+    apiKey: "AIzaSyCcF8iRBSBjonBARmWSy58EG5uLZOpYldU",
+    authDomain: "vandydsc.firebaseapp.com",
+    databaseURL: "https://vandydsc.firebaseio.com",
+    projectId: "vandydsc",
+    storageBucket: "vandydsc.appspot.com",
+    messagingSenderId: "1085611468897",
+    appId: "1:1085611468897:web:cbcfd9c5cabd6496b5a220",
+    measurementId: "G-TTXQJZQD1M"
+};
